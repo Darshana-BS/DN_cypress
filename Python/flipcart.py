f78dee264ec692f845722b10e2f9154038973f49
DarshPoo <darsh@Darshanas-iMac.local>
2024-02-09 17:30:43 +0530

    Flipcart code for selenium using python for search

---
+++ Python/flipcart.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://www.flipkart.com/")
+# searchbox = driver.find_element("xpath","//input[@class = 'Pke_EE']")
+driver.save_screenshot("screenshot-1.png")
+# searchbox.send_keys("Tata teat premium")
+driver.save_screenshot("screenshot-2.png")
+
+driver.find_element("xpath", "//button[@class = '_2iLD__']" ).click()
+
+driver.close() 
