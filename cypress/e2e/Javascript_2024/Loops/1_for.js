184798a49a308f0a026af810f1b5106cfaefcd06
DarshPoo <darsh@Darshanas-iMac.local>
2024-02-12 20:07:48 +0530

    JS code day 1

---
+++ cypress/e2e/Javascript_2024/Loops/1_for.js
@@ -0,0 +1,7 @@
+//for loop
+//print number from 0 to 5
+
+for (var  i =0;i<=5;i++)
+{
+    console.log('number is: ', +i)
+}
---
+++ cypress/e2e/Javascript_2024/Loops/2_for.js
@@ -0,0 +1,7 @@
+//for loop
+//print negative value of the positive num
+
+for (i=0;i<11;i++)
+{
+    console.log('-ve of the positive number is: ', -i)
+}
