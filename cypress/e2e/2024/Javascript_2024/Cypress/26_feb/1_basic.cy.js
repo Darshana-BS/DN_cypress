fb439103d7af7be0bebe83fe800d905a394dde81
DarshPoo <darsh@Darshanas-iMac.local>
2024-02-28 07:12:40 +0530

    28th Feb 1

--- cypress/e2e/2024/Javascript_2024/Cypress/26_feb/1_basic.cy.js
+++ cypress/e2e/2024/Javascript_2024/Cypress/26_feb/1_basic.cy.js
@@ -2,11 +2,16 @@
 
 it('Google test', () => {
 cy.visit('https://google.com')
-cy.get('div .gLFyf').type('Darshana Nehulkar{enter}')
+cy.get('div .gLFyf').type('Automation step by step{enter}')
 // cy.get('[data-hveid="CAwQAA"] > .LatpMc > .GKS7s').click()
-cy.get('div .Tl6lnd').eq(1).click()
+cy.get('.GKS7s').eq(1)
+// cy.contains('Videos').click()
 
 // cy.get('div .ExCKkf').click()
 // cy.get('div .gLFyf').type('{downarrow}').click()
-
+cy.wait(2000)
+cy.log(Cypress.browser)
+close()
 })
+
+
