858d7da48d431a709d668c123b1050b29eb0fd59
DarshPoo <darsh@Darshanas-iMac.local>
2024-03-01 20:18:25 +0530

    1st March

---
+++ cypress/e2e/Javascript_2024/1stMarch/2_variables.js
@@ -0,0 +1,20 @@
+//referebce = https://www.linkedin.com/pulse/javascript-interview-questions-automation-testers-part-thimmaraju-g-3c/
+
+var a                       //undefined: no value is assigned
+console.log(a)
+console.log(typeof(a))
+
+var b = null                //nULL  Null: Null means an empty value or a blank value.
+var d = null                //nULL  Null: Null means an empty value or a blank value.
+console.log(b)
+console.log(typeof(b))
+
+var test = 'DN'
+console.log(`The variable "test" has type as:`, typeof(test))   // The variable "test" has type as
+
+
+console.log(a+b)            //prints NaN when arithmetic operations are performed
+console.log(b+d)            //prints 0 as this converts the value of null to 0 and perform actions
+
+console.log(20+30+"30")
+console.log("30"+20+30)
