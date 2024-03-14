944f50ae5337b6a9859b1765626b9876ac4dc05a
DarshPoo <darsh@Darshanas-iMac.local>
2024-03-14 16:04:20 +0530

    14th fresh commit #1

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,7 @@
-const { defineConfig } = require("cypress");
-
-watchForFileChanges:false
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    watchForFileChanges:false,
-    "reporter": "mochawesome",
-      "reporterOptions": {
-        "charts": true,
-        "overwrite": false,
-        "html": false,
-        "json": true,
-        "reportDir": "cypress/report/mochawesome-report"
-      }
   },
-});
+};
