7aa7e4820d3866dae89a81de6e70a7d00f47719d
DarshPoo <darsh@Darshanas-iMac.local>
2024-04-05 12:50:33 +0530

    5th April 1srr

---
+++ Python/April_4th/OMP.py
@@ -0,0 +1,16 @@
+array = [1,2,3, 4, 5]
+
+smallest = array[0]
+
+for i in array:
+    if i < smallest:
+        smallest = i
+print(smallest)
+
+
+# -ve
+# wallet 100 vali 30 days
+# I should be 5kms
+#2 it location accurate
+#3 1km - 5km
+#4 drop
