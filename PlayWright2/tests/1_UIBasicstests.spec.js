6a71ddb33c47a2fb0a8f5c99192a643ceae062bb
Darsh_CF <darsh_cf@Darsh-CFs-MacBook-Pro.local>
2025-09-17 21:47:31 +0530

    Sept 16th

--- PlayWright2/tests/1_UIBasicstests.spec.js
+++ PlayWright2/tests/1_UIBasicstests.spec.js
@@ -1,15 +1,38 @@
-const {test} = require('@playwright/test');         //test annotation comes from this playwright/test module, hence it is needed 
+const {test, expect} = require('@playwright/test');         //test annotation comes from this playwright/test module, hence it is needed 
+const { trace } = require('console');
+const exp = require('constants');
 
+// trace 
+async function startTracing(context, name) {
+    await context.tracing.start({ screenshots: true, snapshots: true })
+    return async() => {
+        await context.tracing.stop({ path: `trace/${name}.zip` });
+    };
+}
 // test case to be delcared here
-test('Browser Context Playwright Test1', async ({browser})=>        // async is needed, await gets executed else it wont make any difference
+test('TC_01 Browser Context Playwright Test1', async ({browser})=>        // async is needed, await gets executed else it wont make any difference
 {
         //playwright code to be added here
-        const context = browser.newContext();        //create instance for new instance for browser with some set configurations 
-        const page = (await context).newPage;        //creates actual page to automate 
+        const context = await browser.newContext();        //create instance for new instance for browser with some set configurations 
+        const stopTrace = await startTracing(context, 'TC_01');
+        const page = await context.newPage();        //creates actual page to automate 
         await page.goto("http://www.rahulshettyacademy.com/loginpagePractise/");
+        console.log (await page.title());
+        await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+
+        //Locators = CSS, XPATH 
+        await page.locator('#username').fill('rahulshettyacade');
+        await page.locator('#password').fill('learning');
+        await page.locator('#signInBtn').click();
+        console.log (await page.locator("[style*='block']").textContent());             //wait for this element to find 
+        
+        stopTrace();
 });
 
-test('Page Playwright Test2', async({page}) => 
+test('TC_02 Page Playwright Test2', async({page}) => 
 {
-    await page.goto("http://www.rahulshettyacademy.com/loginpagePractise/");
+    await page.goto("http://www.google.com"); 
+    //get title - assertion 
+    console.log (await page.title());
+    await expect(page).toHaveTitle("Google");
 })
