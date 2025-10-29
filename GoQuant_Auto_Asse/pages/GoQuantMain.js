d2a457a369edcebf9b29df338ed8087d5566a5a9
Darsh_CF <darsh_cf@Darsh-CFs-MacBook-Pro.local>
2025-10-29 21:16:16 +0530

    final touches

--- GoQuant_Auto_Asse/pages/GoQuantMain.js
+++ GoQuant_Auto_Asse/pages/GoQuantMain.js
@@ -203,7 +203,8 @@ class GoQuantMain {
     //logout------------------------------------------------------------------------------------------------
     this.userProfile = page.getByRole('button', { name: 'user14@goquant.io' });
     this.logoutButton= page.getByRole('menuitem', { name: 'Sign out' });
-    this.validateLogout = page.getByRole('heading', { name: 'Welcome' })
+    // this.validateLogout = page.getByRole('heading', { name: 'Welcome' });
+    this.validateLogout = page.getByText('Enter your credentials');
     //---------------------------------------------------------------------------------------------------------------
   }
   
@@ -782,7 +783,8 @@ async smartRouting(){
 }
 
 async consolidatedView(){
-  
+  this.getStarted.click();
+  await this.page.getByTestId('consolidated-orderbook-toggle').click();
 }
 
 async placeOKXSellOrder(quantity){
@@ -877,7 +879,8 @@ async logout() {
   await this.getStarted.click();
   await this.userProfile.click();
   await this.logoutButton.click();
-  await expect(this.validateLogout).toHaveText('Welcome');
+  await this.validateLogout;
+  await expect(this.validateLogout).toHaveText('Enter your credentials');
   }
 }
 
